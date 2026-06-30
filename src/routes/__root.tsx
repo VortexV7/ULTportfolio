1afe3acbab9853c7915746cea5a41fd988b638d0
VortexV7 <vedsharanagate05@gmail.com>
2026-06-30 22:02:45 +0530

    Added Vercel Analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,6 +7,7 @@ import {
   HeadContent,
   Scripts,
 } from "@tanstack/react-router";
+import { Analytics } from "@vercel/analytics/react";
 import { type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
@@ -123,6 +124,7 @@ function RootShell({ children }: { children: ReactNode }) {
       </head>
       <body>
         {children}
+        <Analytics />
         <Scripts />
       </body>
     </html>
